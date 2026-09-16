0a5710639782a7475b2a44da3d118a9503dee9e7
JoniKristian82 <joni.tuunanen@student.laurea.fi>
2026-09-16 22:41:48 +0300

    juu

--- Ws2_Js_basics/js/script.js
+++ Ws2_Js_basics/js/script.js
@@ -6,4 +6,4 @@ console.log(name);
 console.log(age);
 console.log(favouriteAnimal);
 
-console.log(`Hello! My name is {name} and my favourite animal is the {favouriteAnimal}.`);
+console.log(`Hello! My name is ${name} and my favourite animal is the ${favouriteAnimal}.`);

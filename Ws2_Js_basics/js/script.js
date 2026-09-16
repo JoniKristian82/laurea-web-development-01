03ef10433be8fefc8b8dafbd058aba73d895df5f
JoniKristian82 <joni.tuunanen@student.laurea.fi>
2026-09-16 23:17:38 +0300

    juu

--- Ws2_Js_basics/js/script.js
+++ Ws2_Js_basics/js/script.js
@@ -28,6 +28,6 @@ const annettuNimi = prompt("Kirjoita nimesi:");
 
 tervehdiKayttajaa(annettuNimi);
 
-function naytaViesti(Se toimii) {
+function naytaViesti() {
     alert("JavaScript toimii!");
 }

24ced674b22b4caafb349a9414745c30302df9df
JoniKristian82 <joni.tuunanen@student.laurea.fi>
2026-09-24 13:12:16 +0300

    juu

--- Workshop3/animal.js
+++ Workshop3/animal.js
@@ -1,3 +1,28 @@
-function click me() {
-    alert("You clicked me");
+function showTable() {
+  const animals = [
+    { animal: "Tiger", habitat: "Forest", diet: "Carnivore" },
+    { animal: "Elephant", habitat: "Savanna", diet: "Herbivore" }
+  ];
+
+  let rows = "";
+  for (const { animal, habitat, diet } of animals) {
+    rows += `
+      <tr>
+        <td>${animal}</td>
+        <td>${habitat}</td>
+        <td>${diet}</td>
+      </tr>`;
+  }
+
+  const table = `
+    <table border="1">
+      <tr>
+        <th>Animal</th>
+        <th>Habitat</th>
+        <th>Diet</th>
+      </tr>
+      ${rows}
+    </table>`;
+
+  document.querySelector("#tableContainer").innerHTML = table;
 }

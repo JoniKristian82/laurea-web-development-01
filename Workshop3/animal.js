27c3e85b964c707d56a06383aaa0a5a06006a79c
JoniKristian82 <joni.tuunanen@student.laurea.fi>
2026-09-24 10:03:29 +0300

    juu

--- Workshop3/animal.js
+++ Workshop3/animal.js
@@ -1,5 +1,32 @@
-const animalButton = document.getElementById("animalButton");
+function showTable() {
+    const animal = "Tiger";
+    const habitat = "Forest";
+    const diet = "Carnivore";
 
-animalButton.addEventListener("click", function() {
-    alert("Tiikerit ovat söpöjä");
-});
+    const animal2 = "Elephant";
+    const habitat2 = "Savanna";
+    const diet2 = "Herbivore";
+
+    const tableHTML = `
+        <table border="1">
+            <tr>
+                <th>Animal</th>
+                <th>Habitat</th>
+                <th>Diet</th>
+            </tr>
+            <tr>
+                <td>${animal}</td>
+                <td>${habitat}</td>
+                <td>${diet}</td>
+            </tr>
+            <tr>
+                <td>${animal2}</td>
+                <td>${habitat2}</td>
+                <td>${diet2}</td>
+            </tr>
+        </table>
+    `;
+
+    const container = document.querySelector("#tableContainer");
+    container.innerHTML = tableHTML;
+}
